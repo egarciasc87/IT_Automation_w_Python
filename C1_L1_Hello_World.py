fb6096046a3345bf7ecbaa06dbca07852250c416
Erick Garcia <egarciasc87@gmail.com>
2022-07-20 21:44:51 +0300

    V1: Hello world

---
+++ C1_L1_Hello_World.py
@@ -0,0 +1,12 @@
+print("Hello, world!!!")
+
+name = input("What's your name? ")
+print("Nice to meet you", name)
+
+number1 = input("Enter number 1: ")
+number2 = input("Enter number 2: ")
+print("Sum is: ", int(number1) + int(number2))
+print("Difference is: ", int(number1) - int(number2))
+print("Multiplication is: ", int(number1) * int(number2))
+print("Division is: ", int(number1) / int(number2))
+print("Power is:", int(number1) ** int(number2))
