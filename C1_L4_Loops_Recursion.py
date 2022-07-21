d639cc9e9db28bd3e0e667fd80cb273993c565a6
Erick Garcia <egarciasc87@gmail.com>
2022-07-21 15:36:25 +0300

    V5: Add more loop examples

--- C1_L4_Loops_Recursion.py
+++ C1_L4_Loops_Recursion.py
@@ -9,6 +9,7 @@ while i<int(number):
     print("Repetition", i)
     i = i +1
 
+
 #example 2: add X numbers to fibonacci list
 fibonacciList = [0, 1]
 number = input("\nHow many number do you want to add? ")
@@ -28,6 +29,39 @@ while i<number:
         print("WARNING: item", newNumber, "will exceed limit!!!")
         break
 
+number = i
 print("Fibonacci list:", fibonacciList)
 print("Length of the list:", len(fibonacciList))
 print("Numbers added:", number)
+
+
+#example3: read item from fibonacciList
+i = 0
+sum = 0
+
+print("\nList of items, one by one:")
+
+for i in range(len(fibonacciList)):
+    sum += fibonacciList[i]
+    print("Item #", str(i+1), ":", fibonacciList[i])
+
+print("Sum of items of the list:", str(sum))
+
+
+#example4: read limited number of items
+start = input("\nStart index: ")
+start = int(start)
+end = input("End index: ")
+end = int(end)
+
+for i in range(start, end):
+    print("Item #" + str(i+1) + ":", fibonacciList[i])
+
+
+#example 5: list of matches in a football league
+teamsList = ["Sporting Cristal", "Alianza Lima", "Universitario", "Manucci", "UCV", "Melgar"]
+
+for local in teamsList:
+    for visitor in teamsList:
+        if local != visitor:
+            print(local, "VS", visitor)
